620ecbaf7795f570a0b7f036643b4ff4b36f91da
superatmel <superatmel@gmail.com>
2014-09-01 04:42:50 +0200

    Prepare for OSR
    Change-Id: I12f1a75b39ea510eaab05150457f80dc8813a6bb

--- include/private/android_filesystem_config.h
+++ include/private/android_filesystem_config.h
@@ -78,6 +78,8 @@
 #define AID_SDCARD_ALL    1035  /* access all users external storage */
 #define AID_AUDIT         1036  /* audit daemon */
 
+#define AID_THEMEMAN      1300  /* theme manager */
+
 #define AID_SHELL         2000  /* adb and debug shell user */
 #define AID_CACHE         2001  /* cache access */
 #define AID_DIAG          2002  /* access to diagnostic resources */
@@ -204,6 +206,7 @@ static const struct android_id_info android_ids[] = {
 
     { "misc",          AID_MISC, },
     { "nobody",        AID_NOBODY, },
+    { "theme_man", AID_THEMEMAN },
     { "audit",      AID_AUDIT, },
 };
 
